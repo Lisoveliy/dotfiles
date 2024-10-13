9473210776dc0b859e7b24d894234b0d24140e3b
Савелий Савенок <ssavenok@llc.local>
2024-10-14 00:57:59 +0300

    cleanup

--- polybar/launch-poly.sh
+++ polybar/launch-poly.sh
@@ -3,7 +3,5 @@
 
 killall -q polybar
 echo "---" | tee -a /tmp/example_bar.log # Просмотр логов
-polybar standard >> /tmp/standard_polybar.log # Запуск бара examstandardple и запись его лога
-if (xrandr -q | grep -E '\bDP-1 connected') then
-    polybar external >> /tmp/external_polybar.log # Запуск бара example и запись его лога
-fi
+polybar standard >> /tmp/standard_polybar.log # Запуск бара standard и запись его лога
+polybar external >> /tmp/external_polybar.log # Запуск бара example и запись его лога
